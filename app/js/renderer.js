5e5183d1f2a065d96ebb2fa9b69f3f13f191ff65
andre <andre.ferreira.alves@gmail.com>
2019-05-04 15:07:23 -0300

    alterações no botão de inicio e parada

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,8 +1,26 @@
 const { ipcRenderer } = require('electron');
+const timer = require('./timer');
 
 let linkSobre = document.querySelector('#link-sobre');
+let tempo = document.querySelector('.tempo');
+let botaoPlay = document.querySelector('.botao-play');
 
+let play = false;
+let imgs = ['img/play-button.svg', 'img/stop-button.svg'];
 
-linkSobre.addEventListener('click' , function(){
+linkSobre.addEventListener('click', () => {
     ipcRenderer.send('abrir-janela-sobre');
+});
+
+botaoPlay.addEventListener('click', () => {
+    if (play) {
+        timer.parar();
+        play = false;
+    } else {
+        timer.iniciar(tempo);
+        play = true;
+    }
+
+    imgs = imgs.reverse();
+    botaoPlay.src = imgs[0];
 });
